ba7e9f00859f6b3e4692b9a98e289b5f2f0992a9
mastermind <adebolaogunsanya75@gmail.com>
2025-10-17 10:45:28 -0700

    initial commit

---
+++ src/message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    const name = 'Mastermind';
+    if (name)
+        return <h1>Hello {name}</h1>;
+        return <h1>Hello World</h1>;
+        return <h1>Hello bola</h1>;
+}
+export default Message;
