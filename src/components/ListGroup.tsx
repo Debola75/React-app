6c87f4b9718a59b61e93be80e48e4c6fbf6e408c
mastermind <adebolaogunsanya75@gmail.com>
2025-10-20 13:47:18 -0700

    initial commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
